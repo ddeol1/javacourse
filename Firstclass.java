2f3ef14dbbce00aab230c55bf35692bc0b5d2b95
Deeshu Deol <nitindeolnd4@gmail.com>
2024-09-21 18:41:42 +0530

    Add files via upload

---
+++ Firstclass.java
@@ -0,0 +1,14 @@
+package javalearning;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 12;
+		int c = a + b;
+		System.out.println(c);
+		
+   
+	}
+
+}
